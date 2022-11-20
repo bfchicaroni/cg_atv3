ea315c5767e5aa588c98c758c77785d288879173
bfchicaroni <bia.f.chicaroni@uol.com.br>
2022-11-20 13:11:46 -0300

    Ajustando FPS local para sincronizar a execução local com a web

--- examples/sistemaSolar/main.cpp
+++ examples/sistemaSolar/main.cpp
@@ -5,7 +5,8 @@ int main(int argc, char **argv) {
     abcg::Application app(argc, argv);
 
     Window window;
-    window.setOpenGLSettings({.samples = 4});
+    window.setOpenGLSettings(
+        {.samples = 4, .vSync = true, .doubleBuffering = true});
     window.setWindowSettings({
         .width = 600,
         .height = 600,
